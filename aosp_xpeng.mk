d323975db6aac18dd15ada86b6039402f2066e25
Fraaxius <franthekillaz@gmail.com>
2024-08-24 13:29:28 -0400

    xpeng: Let's go, PixelOS.

--- aosp_xpeng.mk
+++ aosp_xpeng.mk
@@ -11,10 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from xpeng device
 $(call inherit-product, device/motorola/xpeng/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_xpeng
+# PixelOS Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+PRODUCT_NAME := aosp_xpeng
 PRODUCT_DEVICE := xpeng
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
